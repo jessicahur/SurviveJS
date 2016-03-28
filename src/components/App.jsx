274f02b75cabf758ff9f18221089f01d27307b69
Jessica Tran Hur <jesshur824@gmail.com>
2016-03-28 15:40:35 -0500

    first React app running without a warning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
-import Note from './Note.jsx';
+import uuid from 'node-uuid';
+
 
 export default class App extends React.Component {
   render() {
-    return <Note />;
+    const notes = [
+      {
+        id: uuid.v4(),
+        task: 'Learn Webpack'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Learn React'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Do laundry'
+      }
+    ];
+
+    return(
+      <div>
+        <ul> {notes.map(note => <li key={note.id}>{note.task}</li> )}
+        </ul>
+      </div>
+    );
   }
 }
+
+/*Notes:
+In order to tell React in which order to render the elements, we use the key property.
+It is important that this is unique or else React won't be able to figure out the correct order in which to render. If not set, React will give a warning.
+*/
+
