08a6ca2a8e12467ff67674a22e8d4d56eb93f439
Jessica Tran Hur <jesshur824@gmail.com>
2016-03-28 14:57:46 -0500

    simple React App

---
+++ src/components/Note.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => <div>Learn Webpack</div>;
+
+/*Note: Even though we aren't referring to React directly through code here, it is good to remember
+that the JSX will get transformed into calls going through it.
+Hence if you remove the import statement, the code will break.
+Babel plugin known as babel-plugin-react-require is able to generate the imports for you automatically if you prefer to avoid the imports.
+*/
