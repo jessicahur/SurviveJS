1f9bd9fe6aa0904550038ec0d57fb0d2f9f9009f
Jessica Tran Hur <jesshur824@gmail.com>
2016-03-31 15:26:02 -0500

    have Note class run for editting

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,6 +1,70 @@
 import React from 'react';
 
-export default ({task}) => <div>{task}</div>;
+export default class Note extends React.Component{
+  constructor(props) {
+    super(props);
+
+    //Track editing state
+    this.state = {
+      editing: false
+    };
+  }
+
+  //No need for ',' to separate properties (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes)
+  render() {
+    //Render the component differently based on state
+    if( this.state.editing) {
+      return this.renderEdit();
+    }
+    return this.renderNote();
+  }
+
+  //react lifecycle methods are automatically bound to “this" whereas custom ones require doing so manually
+  // fat arrow is a bit of a hack because they don’t create a new scope, in the same way that function() does.
+  //(@danielle Portland-ReactJS Slack channel)
+  renderEdit = () => {
+    return <input type='text'
+      ref={
+        //anonymous function that takes an event. If event, return e.selectionStart = props.task.length. Otherwise return null
+        (e) => e ? e.selectionStart = this.props.task.length : null
+      }
+      autoFocus={true}
+      defaultValue={this.props.task}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter} />
+  };
+
+  renderNote = () => {
+    return <div onClick={this.edit}>{this.props.task}</div>;
+  };
+
+  edit = () => {
+    this.setState({
+      editing: true
+    });
+  };
+
+  checkEnter = (e) => {
+    //If the user hit *enter*. we finish
+    if(e.key === 'Enter') {
+      this.finishEdit(e);
+    }
+  };
+
+  finishEdit = (e) => {
+    const value = e.target.value;
+
+    if(this.props.onEdit) {
+      this.props.onEdit(value);
+
+      //Exit edit mode
+      this.setState({
+        editing: false
+      });
+    }
+  };
+}
+
 
 /*Note: Even though we aren't referring to React directly through code here, it is good to remember
 that the JSX will get transformed into calls going through it.
